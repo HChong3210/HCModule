95c73c85e93964563bed892504c62bdce12d2cc7
HChong3210 <hchong7557@gmail.com>
2017-07-19 22:35:16 +0800

    first commit
    Signed-off-by: HChong3210 <hchong7557@gmail.com>

---
+++ HCModule/HCModule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCModule
+//
+//  Created by HChong on 2017/7/19.
+//  Copyright © 2017年 HChong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
